bf2973f204a7a516ce9164d78a0f7911fb2c75ea
Iron11 <chocowinners@gmail.com>
2025-04-10 16:23:01 -0400

    A City of Code...
    Updated Battle class

--- code/Battle.java
+++ code/Battle.java
@@ -1,30 +1,36 @@
 import java.util.Scanner;
-public class Battle{
-    public void battler(Entity player,Entity enemy){
-        Scanner input = new Scanner(System.in);
-        int coins = 1, abilityUses = 1;//Temporary
-        String skillClass="mage";
-        System.out.println(enemy.getName()+" appeared!");
+public class Battle{//Temporary
+    public Scanner input = new Scanner(System.in);
+    public int coins = 1, abilityUses; //Coins is Temporary
+    public boolean battler(Entity player,Entity enemy){
+        System.out.println(enemy.getName() + " appeared!");
+        String skillClass = "mage"; //Temporary
+        boolean skip;
+        abilityUses = 1;
         while((player.getHp()>0)&&(enemy.getHp()>0)){
+            game.clear();
             System.out.print("Options:\n1. Attack\n2. Bag\n3. Use Ability\n4. Run");
-            switch(Integer.parseInt(input.nextline())) {
+            skip = false;
+            switch(Integer.parseInt(input.nextLine())) {
                 case 1://Battle
                     battleCalculator(player, enemy);
                     break;
                 case 2://Bag
                     break;
                 case 3://Ability
-                    if(abilityUses>0){
+                    if(abilityUses > 0){
                         switch(skillClass){
                             case "mage":
                                 enemy.setHp(enemy.getHp()/2.0);
                                 break;
-                            case "barb": break;
                             default: break;
                         }
                         abilityUses--;
                     }
-                    else System.out.println("You have no ability uses!");
+                    else{
+                        System.out.println("You have no ability uses!");
+                        skip = true;
+                    }
                     break;
                 case 4://Run
                     player.setHp(0);
@@ -32,28 +38,30 @@ public class Battle{
                 default:
                     break;
             }
-            if(player.getHp()<=0) {
+            if(skip) continue;
+            if(player.getHp() <= 0) {
                 System.out.printf("You lost and dropped %d coins!",coins);
-                coins-=2;
-                break;
+                coins -= 2;
+                return true;
             }
             battleCalculator(enemy,player);
-            if(enemy.getHp()<=0) {
+            if(enemy.getHp() <= 0) {
                 System.out.printf("You defeated %s and they dropped %d coins!",enemy.getName(),coins);
-                coins+=2;
-                break;
+                coins += 2;
+                return false;
             }
         }
+        return false; //Fallback
     }
     public void battleCalculator(Entity attacker,Entity defender){
         double damage = attacker.getAtk();
-        if(Math.random()<=attacker.getCritChance()){
+        if(Math.random() <= attacker.getCritChance()){
             System.out.println("Critical Hit!");
             damage *= 1.5;
         }
-        if(Math.random()<=attacker.getDodgeChance()) System.out.println("Attack dodged!");
-        else if(damage<=defender.getDef()) defender.setHp(defender.getHp() - 1.0);
+        if(Math.random() <= attacker.getDodgeChance()) System.out.println("Attack dodged!");
+        else if(damage <= defender.getDef()) defender.setHp(defender.getHp() - 1.0);
         else defender.setHp(defender.getHp() - (damage - defender.getDef()));
-        if(defender.getHp()<=0) defender.setHp(0);
+        if(defender.getHp() <= 0) defender.setHp(0);
     }
 }
