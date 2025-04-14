318cff4431e75ee06915e89f9c79e14dc66fff68
Iron11 <chocowinners@gmail.com>
2025-04-14 15:56:25 -0400

    A Province of Code...
    Updated battle class and added coin value to enemies.

--- code/Battle.java
+++ code/Battle.java
@@ -1,25 +1,26 @@
 import java.util.Scanner;
 public class Battle{//Temporary
     public static Scanner input = new Scanner(System.in);
-    public static int coins = 1, abilityUses; //Coins is Temporary
-    public static void battler(Player player, Entity enemy){
+    public static int abilityUses; //Coins is Temporary
+    public static void battler(Player player,SmallEnemy enemy){
         System.out.println(enemy + " appeared!");
-        String skillClass = "mage", loseText = "defeated" + enemy; //Temporary
+        String skillClass = "mage"/*Temporary*/,loseText = "defeated" + enemy;
         boolean skip, run;
-        int win = 0;
+        int coins = enemy.getCoinValue(), win = 0;
         abilityUses = 1;
-        while(win == 0){
+        while(win==0){
             game.clear();
             System.out.printf("%s has %.1f/%.1f HP remaining.%n%s has %.1f/%.1f HP remaining.%nOptions:%n1. Attack%n2. Bag%n3. Use Ability%n4. Run%n", player, player.getHp(), player.getMaxHp(), enemy, enemy.getHp(), enemy.getMaxHp());
             skip = false;
             run = false;
             switch(Integer.parseInt(input.nextLine())) {
                 case 1://Battle
-                    //battleCalculator(player, enemy);
                     win=battleEnemy(player,enemy);
                     break;
                 case 2://Bag
                     System.out.println(player.getInv());
+                    battleCalculator(enemy,player);
+                    if(player.getHp() <= 0) win = -1;
                     break;
                 case 3://Ability
                     if(abilityUses > 0){
@@ -31,9 +32,7 @@ public class Battle{//Temporary
                         }
                         abilityUses--;
                         battleCalculator(enemy,player);
-                        if(player.getHp() <= 0) {
-                            win = -1;
-                        }
+                        if(player.getHp() <= 0) win = -1;
                     }
                     else{
                         System.out.println("You have no ability uses!");
@@ -49,33 +48,25 @@ public class Battle{//Temporary
             }
             if(skip) continue;
             if(win==1){
-                System.out.printf("You defeated %s! They dropped %d coins.",enemy, 2);
-                coins += 2;
+                System.out.printf("You defeated %s! They dropped %d coins!",enemy,coins);
+                player.setBal(player.getBal()+coins);
             }else if(win==-1){
-                System.out.printf("You %s and dropped %d coins!",loseText, 2);
-                coins -= 2;
+                System.out.printf("You %s and you dropped %d coins!",loseText,coins);
+                player.setBal(player.getBal()-coins);
             }
         }
     }
     public static int battleEnemy(Entity player, Entity enemy){
         if(player.getSpd() >= enemy.getSpd()){
             battleCalculator(player, enemy);
-            if(enemy.getHp() <= 0) {
-                return 1;
-            }
+            if(enemy.getHp() <= 0) return 1;
             battleCalculator(enemy,player);
-            if(player.getHp() <= 0) {
-                return -1;
-            }
+            if(player.getHp() <= 0) return -1;
         }else{
             battleCalculator(enemy,player);
-            if(player.getHp() <= 0) {
-                return -1;
-            }
+            if(player.getHp() <= 0) return -1;
             battleCalculator(player, enemy);
-            if(enemy.getHp() <= 0) {
-                return 1;
-            }
+            if(enemy.getHp() <= 0) return 1;
         }
         return 0;
     }
@@ -93,7 +84,7 @@ public class Battle{//Temporary
             }
             else {
                 defender.setHp(defender.getHp() - (damage - defender.getDef()));
-                System.out.printf("%s dealt %.0f HP to %s!",attacker,(damage - defender.getDef()),defender);
+                System.out.printf("%s dealt %.0f HP to %s!%n",attacker,(damage - defender.getDef()),defender);
             }
         }
         if(defender.getHp() <= 0) defender.setHp(0);
--- code/SmallEnemy.java
+++ code/SmallEnemy.java
@@ -1,5 +1,11 @@
 public class SmallEnemy extends Entity {
+    private int coinValue;
     SmallEnemy(String name, int maxHp, int atk, int def, int spd) {
         super(name, maxHp, atk, def, spd);
+        coinValue=1;
+    }public int getCoinValue(){
+        return coinValue;
+    }public void setCoinValue(int coinValue){
+        this.coinValue=coinValue;
     }
 }
